9981c3644922334263f303a6722ba62ecd47bb6c
Brynna Miller <brynnam@email.unc.edu>
2023-04-23 23:32:56 -0400

    finishing up the rest of the program

--- cli.js
+++ cli.js
@@ -6,6 +6,16 @@ import moment from "moment-timezone";
 const args = minimist(process.argv.slice(2));
 const timezone = moment.tz.guess()
 
+const url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&timezone=" + timezone + "&daily=precipitation_hours";
+const response = await fetch(url)
+const data = await response.json()
+
+const days = args.d;
+
+let latitude;
+let longitude;
+let message; 
+
 if (args.h) {
     console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
         -h            Show this help message and exit.
@@ -16,4 +26,54 @@ if (args.h) {
         -j            Echo pretty JSON from open-meteo API and exit.
     `);
     process.exit(0);
+}
+
+// storing lat + long
+
+if(args.n && args.s) {
+    console.log("Please only input one latitude argument. Either north (n) or south (s).");
+    process.exit(0);
+}
+else if(args.n) {
+    latitude = args.n;
+} else if(args.s) {
+    latitude = -args.s;
+} else {
+    console.log("Latitude must be in range");
+    process.exit(0);
+
+} if(args.w && args.e) {
+    console.log("Please only input one longitude argument. Either west (w) or east (e).");
+    process.exit(0);
+} else if(args.e) {
+    longitude = args.e;
+} else if(args.w) {
+    longitude = -args.w;
+} else {
+    console.log("Please input a longitude argument. Either north west (w) or east (e).");
+    process.exit(0);
+} 
+
+
+
+if (data.daily.precipitation_hours[days] > 0) {
+    message = "Grab an umbrella "
+} else {
+    message = "It's a great day "
+}
+
+if (days == 0) {
+    message += "today.";
+} else if (days > 1) {
+    message += "in " + days + " days.";
+} else {
+    message += "tomorrow.";
+}
+
+if (args.j) {
+    console.log(data);
+    process.exit(0)
+} 
+else {
+    console.log(message);
 }
