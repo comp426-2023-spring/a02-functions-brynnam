5ffb730057774b60c21c16f0b601cd9f901b031e
Brynna Miller <brynnam@email.unc.edu>
2023-04-29 21:33:39 -0400

    simplification to fix issues

--- cli.js
+++ cli.js
@@ -1,72 +1,41 @@
 #!/usr/bin/env node
+
 import minimist from 'minimist';
 import fetch from 'node-fetch';
-import moment from 'moment-timezone';
+import moment from "moment-timezone";
 
 const args = minimist(process.argv.slice(2));
-
-if (args.h); {
-    console.log(
-        `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-            -h            Show this help message and exit.
-            -n, -s        Latitude: N positive; S negative.
-            -e, -w        Longitude: E positive; W negative.
-            -z            Time zone: uses tz.guess() from moment-timezone by default.
-            -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-            -j            Echo pretty JSON from open-meteo API and exit.`
-        );
+if (args.h) {
+    console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.`
+    );
     process.exit(0);
 }
-  
-  async function getWeatherData(latitude, longitude, timezone, day) {
 
-    if (!latitude) {
-      console.log('Latitude must be in range')
-      return;
-    }
 
-    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=${timezone}`
-  
-    if ((!latitude || !longitude) && !args.j) {
-        console.error('please provide both a latitude and longitude.');
-        printHelp();
-      }
+const timezone = moment.tz.guess();
+var latitude = args.n || -1 * args.s;
+var longitude = args.e || -1 * args.w;
+const days = args.d;
 
-    try {
-      // grabbing API data
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      if (args.j) {
-        console.log(JSON.stringify(data, null, 2));
-        process.exit(0);
-      }
-  
-      // give weather for specific day
-      return data.daily.precipitation_hours[day];
-    } catch (error) {
-      console.error('Error fetching weather data:', error);
-      process.exit(1);
-    }
-  }
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&temperature_unit=fahrenheit&timezone=' + timezone);
+const data = await response.json();
 
-  async function main() {
-    const latitude = args.n || args.s;
-    const longitude = args.e || args.w;
-    const timezone = args.z || moment.tz.guess();
-    const day = args.d !== undefined ? args.d : 1;
+if (args.j){
+    console.log(data);
+    process.exit(0);
+}
 
-  
-    // get weather and print
-    const precipitationHours = await getWeatherData(latitude, longitude, timezone, day);
-    console.log(precipitationHours > 0 ? "You might need your galoshes " : "You will not need your galoshes ");
-  
-    if (day === 0) {
-      console.log("today.");
-    } else if (day > 1) {
-      console.log("in " + day + " days.");
-    } else {
-      console.log("tomorrow.");
-    }
-  }
-  
-  main();
+
+if (days == 0){
+    console.log("today.");
+} else if (days > 1){
+    console.log("in" + days + "days.");
+} else {
+    console.log("tomorrow.");
+}
